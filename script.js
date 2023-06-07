494b9d7804ccaa14a3ef677da7e2a775e2e2f802
Deependra Yadav <deependrayadav6306@gmail.com>
2023-06-08 03:36:54 +0530

    resumeButton

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Programmer", "Developer", "Designer"],
+        strings: ["Programmer", "Java-Backend-Developer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Programmer", "Developer", "Designer"],
+        strings: ["Programmer", "Java-Backend-Developer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
